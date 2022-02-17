87908aaedc952144a346eb2f8ebf56e899dcefe7
luciobx <luc.vincent@ac-bordeaux.fr>
2022-02-17 10:29:49 +0100

    Create exo1_corr.py

---
+++ Marquestaut/EP1/exo1_corr.py
@@ -0,0 +1,16 @@
+﻿# Votre code
+def amplitude(valeurs):
+    assert len(valeurs) > 0, "tableau vide"
+    mini = valeurs[0]
+    maxi = valeurs[0]  ## Ne pas initialiser à 0 !!
+    for element in valeurs :          ## Parcours par élément car pas besoin de l'indice
+        if element < mini :
+            mini = element
+        if element > maxi :
+            maxi = element
+    return maxi - mini
+
+tableau = [0, 1, 4, 2, -2, 9, 3, 1, 7, 1]
+
+assert amplitude(tableau) == 11
+assert amplitude([])
