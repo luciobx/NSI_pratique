ef8ce82cb50991b9e8fb1107f0a004d112f09246
luciobx <52229716+luciobx@users.noreply.github.com>
2022-02-17 10:06:31 +0100

    Add files via upload

---
+++ Nouveau dossier/exo2.py
@@ -0,0 +1,32 @@
+﻿class Noeud:
+    def __init__(self,mot,gauche = None,droite = None):
+        self.gauche = gauche
+        self.droite = droite
+        self.mot = mot
+
+def inserer(ABR,mot):
+    '''Insère un élément dans l'arbre binaire de recherche.'''
+    if ABR is None :
+        ABR = ...
+    elif mot < ABR.mot :
+        ABR.gauche = ...
+    elif  mot > ABR.mot :
+        ABR.droite = ...
+    return ABR
+
+
+
+def afficher(ABR):
+    '''Affiche les mots par ordre alphabétique.'''
+    if ABR ... :
+        afficher(...)
+        print(...)
+        afficher(...)
+
+arbre = None
+arbre = inserer(arbre,"kiwi")
+arbre = inserer(arbre,"pomme")
+arbre = inserer(arbre,"abricot")
+arbre = inserer(arbre,"mangue")
+arbre = inserer(arbre,"poire")
+afficher(arbre)
