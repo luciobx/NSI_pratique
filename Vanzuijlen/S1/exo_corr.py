0018617d53560889c8a7283bb73a238709d46522
luciobx <luc.vincent@ac-bordeaux.fr>
2022-02-18 18:57:04 +0100

    Update exo_corr.py

--- Vanzuijlen/S1/exo_corr.py
+++ Vanzuijlen/S1/exo_corr.py
@@ -1,29 +1,33 @@
 class Noeud:
 
-    def __init__(self, valeur=None, gauche = None, droite = None):
+    def __init__(self, valeur=None, gauche=None, droite=None):
         self.valeur = valeur
         self.gauche = gauche
         self.droite = droite
  
     def estFeuille(self):
-        if not self.gauche and not self.droite:
-            return True
-        else:
-            return False
+        return self.gauche is None and  self.droite is None
 
 # partie à modifier
-            
-def somme_maxi(arbre):
+
+def poids_maxi_branche(arbre):
+    if arbre == None:
+        return 0
+    else :
+        return arbre.valeur + max(poids_maxi_branche(arbre.gauche),poids_maxi_branche(arbre.droite))
+
+
+def poids_maxi_branche2(arbre):
     if arbre.estFeuille():
         return arbre.valeur
     elif arbre.gauche == None:
-        return arbre.valeur + somme_maxi(arbre.droite)
+        return arbre.valeur + poids_maxi_branche(arbre.droite)
     elif arbre.droite == None:
-        return arbre.valeur + somme_maxi(arbre.gauche)
+        return arbre.valeur + poids_maxi_branche(arbre.gauche)
     else:
-        return arbre.valeur + max(somme_maxi(arbre.gauche),somme_maxi(arbre.droite))
+        return arbre.valeur + max(poids_maxi_branche(arbre.gauche),poids_maxi_branche(arbre.droite))
 
-    
+# déclaration de l'arbre
 onze = Noeud(11)
 neuf = Noeud(9)
 trente = Noeud(30)
@@ -34,8 +38,14 @@ sept = Noeud(7,trente,quarante)
 vingtetun = Noeud(21,dixsept,huit)
 douze = Noeud(12,sept,vingtetun)
 
-# test 
-assert somme_maxi(douze) == 70
-assert somme_maxi(trente) == 30
-assert somme_maxi(sept) == 58
-assert somme_maxi(vingtetun) == 47
+# test première fonction
+assert poids_maxi_branche(douze) == 70
+assert poids_maxi_branche(trente) == 30
+assert poids_maxi_branche(sept) == 58
+assert poids_maxi_branche(vingtetun) == 47
+
+# test seconde fonction
+assert poids_maxi_branche2(douze) == 70
+assert poids_maxi_branche2(trente) == 30
+assert poids_maxi_branche2(sept) == 58
+assert poids_maxi_branche2(vingtetun) == 47
